67042552f961333c5ad0482de778b2e8a9b638de
한지희, Jihee Han <hatrix1014@gmail.com>
2024-12-20 17:00:18 +0900

    Created profile layout

--- utils/links.tsx
+++ utils/links.tsx
@@ -5,10 +5,8 @@ type NavLink = {
 
 export const links: NavLink[] = [
     { href: '/', label: 'home' },
-    { href: '/mood ', label: 'm' },
-    { href: '/bookings ', label: 'bookings' },
-    { href: '/reviews ', label: 'reviews' },
-    { href: '/rentals/create ', label: 'create rental' },
-    { href: '/rentals', label: 'my rentals' },
+    { href: '/mood ', label: 'mood' },
+    { href: '/bankingaccount ', label: 'bankingaccount' },
     { href: '/profile ', label: 'profile' },
+    { href: '/service ', label: 'service' },
 ];
