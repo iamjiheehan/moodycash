da19a84f546f8dbc1c1d50da6057f9c4da92af50
한지희, Jihee Han <hatrix1014@gmail.com>
2024-12-26 02:17:06 +0900

    backup process

--- components/mood/MoodCalendar.tsx
+++ components/mood/MoodCalendar.tsx
@@ -3,48 +3,45 @@ import { Calendar } from '../ui/calendar';
 import { chartData } from './MoodMockData';
 
 export default function MoodCalendar() {
-    const [selectedDates, setSelectedDates] = useState<Date[]>([]);
-    const [selectedData, setSelectedData] = useState<any>(null);
+    const [selectedDate, setSelectedDate] = useState<Date | undefined>(
+        new Date()
+    );
+    const [selectedData, setSelectedData] = useState<any | undefined>(
+        undefined
+    );
 
-    function handleDateChange(dates: Date[] | undefined) {
-        if (dates) {
-            setSelectedDates(dates);
-            const selectedDateStrings = dates.map(
-                (date) => date.toISOString().split('T')[0]
-            );
-            const data = chartData.filter((item) =>
-                selectedDateStrings.includes(item.date)
+    const handleDateChange = (date: Date | undefined) => {
+        setSelectedDate(date);
+        if (date) {
+            const selectedDateString = date.toISOString().split('T')[0];
+            const data = chartData.find(
+                (item) => item.date === selectedDateString
             );
             setSelectedData(data);
         } else {
-            setSelectedDates([]);
-            setSelectedData(null);
+            setSelectedData(undefined);
         }
-    }
+    };
 
-    // Convert chartData dates to Date objects
     const initialSelectedDates = chartData.map((item) => new Date(item.date));
 
     return (
         <section className="flex flex-col items-center">
             <Calendar
-                mode="multiple"
-                selected={initialSelectedDates}
+                mode="single"
+                selected={selectedDate}
                 onSelect={handleDateChange}
                 className="rounded-md border shadow"
             />
             <div>선택된 날짜를 클릭해보세요</div>
-            {selectedData && selectedData.length > 0 && (
+            {selectedDate && selectedData && (
                 <section>
-                    {selectedData.map((data: any, index: number) => (
-                        <div key={index} className="mt-4">
-                            <div>{data.date}</div>
-                            <div>
-                                {data.amount} 원 만큼{data.feeling}한 날날
-                            </div>
-                            <div>메모: {data.memo}</div>
-                        </div>
-                    ))}
+                    <div>{selectedData.date}</div>
+                    <div>
+                        {selectedData.amount} 원 만큼
+                        {selectedData.mood}한 날
+                    </div>
+                    <div>메모: {selectedData.memo}</div>
                 </section>
             )}
         </section>
