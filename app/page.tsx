881cae6821cc2aea88114a77d397d3231ed43659
한지희, Jihee Han <hatrix1014@gmail.com>
2024-12-25 15:20:35 +0900

    Created LandingPage Sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,71 @@
+import SectionForm from '@/components/form/SectionForm';
 import { Button } from '@/components/ui/button';
 
 function HomePage() {
     return (
         <section>
             <section>
-                <section className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-8">
-                    <section className="flex flex-col justify-center gap-4">
-                        <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-                            Track Your Mood,
-                            <br /> Fund Your Emotions
-                        </h1>
-                        <h2>
-                            Track your emotional journey by depositing into
-                            different accounts
-                            <br /> that reflect your mood. Whether you&apos;re
-                            feeling upbeat or down, <br />
-                            you can visualize how your emotions influence your
-                            financial flow.
-                        </h2>
-                    </section>
-                    <section>
-                        <div>Image section</div>
-                    </section>
-                </section>
-                <Button variant="outline" size="lg" className="capitalize">
-                    Click me
-                </Button>
+                <WelcomeSection />
             </section>
             <section className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-8">
                 <section>
                     <div>그래프 이미지</div>
                 </section>
-                <section className="flex flex-col justify-center gap-4">
-                    <h2>
-                        Deposit money into your desired account based on your
-                        mood.
-                    </h2>
-                    <h2>
-                        Check the account balance through a graph
-                        <br /> and easily track your emotional changes at a
-                        glance.
-                    </h2>
-                </section>
+                <SectionForm
+                    subtitle="Deposit money into your desired account based on your
+                        mood."
+                    additionalSubtitle={[
+                        'Check the account balance through a graph',
+                        'and easily track your emotional changes at a glance.',
+                    ]}
+                />
             </section>
             <section className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-8">
-                <section className="flex flex-col justify-center gap-4">
-                    <h2>How are you feeling today?</h2>
-                </section>
+                <SectionForm subtitle="How are you feeling today?" />
                 <section>
-                    <div>기분 체크</div>
+                    <div>이미지</div>
                 </section>
             </section>{' '}
             <section className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-8">
-                <section className="flex flex-col justify-center gap-4">
-                    <h2>Transfer to your Happy Account !</h2>
-                </section>
+                <SectionForm subtitle="Transfer to your Happy Account !" />
                 <section>
-                    <div>계좌 등록</div>
+                    <div>이미지</div>
                 </section>
             </section>{' '}
             <section className="flex flex-col justify-center gap-4 items-center">
-                <section className="flex flex-col justify-center gap-4">
-                    <h2>Record your mood with a simple note</h2>
-                </section>
-                <Button variant="outline" size="lg" className="capitalize">
-                    Start Using the Service
-                </Button>
+                <SectionForm
+                    subtitle="Record your mood with a simple note"
+                    button="Start Using the Service"
+                />
             </section>
         </section>
     );
 }
+
+const WelcomeSection = () => {
+    return (
+        <section className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-8">
+            <section className="flex flex-col items-start gap-4">
+                <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
+                    Track Your Mood,
+                    <br /> Fund Your Emotions
+                </h1>
+                <h2>
+                    Track your emotional journey by depositing into different
+                    accounts
+                    <br /> that reflect your mood. Whether you&apos;re feeling
+                    upbeat or down, <br />
+                    you can visualize how your emotions influence your financial
+                    flow.
+                </h2>
+                <Button variant="outline" size="lg" className="capitalize">
+                    Click me
+                </Button>
+            </section>
+            <section>
+                <div>Image section</div>
+            </section>
+        </section>
+    );
+};
 export default HomePage;
---
+++ components/form/SectionForm.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import { Button } from '../ui/button';
+import { add } from 'date-fns';
+import React from 'react';
+
+type SectionFormProps = {
+    image?: string;
+    title?: string;
+    subtitle?: string;
+    additionalSubtitle?: string[];
+    button?: string;
+};
+
+const SectionForm = ({
+    image,
+    title = '',
+    subtitle,
+    additionalSubtitle,
+    button,
+}: SectionFormProps) => {
+    return (
+        <section className="flex flex-col justify-center gap-4 items-center">
+            {image && (
+                <Image
+                    src={image}
+                    alt={title}
+                    width={500}
+                    height={300}
+                    className="w-full h-auto"
+                />
+            )}
+
+            <section className="flex flex-col justify-center gap-4">
+                {title && <h1>{title}</h1>}
+                {subtitle && <h2>{subtitle}</h2>}
+                {additionalSubtitle &&
+                    additionalSubtitle.map((text, index) => (
+                        <h2 key={index}>
+                            {text.split('\n').map((line, index) => (
+                                <React.Fragment key={index}>
+                                    {line}
+                                    <br />
+                                </React.Fragment>
+                            ))}
+                        </h2>
+                    ))}
+            </section>
+            {button && (
+                <Button variant="outline" size="lg" className="capitalize">
+                    {button}
+                </Button>
+            )}
+        </section>
+    );
+};
+
+export default SectionForm;
