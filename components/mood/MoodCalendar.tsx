d1c818358f1544020fccee3ab29c362362ad53a0
한지희, Jihee Han <hatrix1014@gmail.com>
2024-12-28 01:42:37 +0900

    Created Displaying data on Calandar logic

--- components/mood/MoodCalendar.tsx
+++ components/mood/MoodCalendar.tsx
@@ -1,45 +1,47 @@
-import React, { useState } from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import { Calendar } from '../ui/calendar';
 import { chartData } from './MoodMockData';
+import { DayMouseEventHandler } from 'react-day-picker';
 
 export default function MoodCalendar() {
-    const [selectedDate, setSelectedDate] = useState<Date[] | undefined>([]); // 초기값을 빈 배열로 설정
+    const [selectedDate, setSelectedDate] = useState<Date[]>([]);
     const [selectedData, setSelectedData] = useState<any | undefined>(
         undefined
     );
 
-    const handleDateChange = (dates: Date[] | undefined) => {
-        setSelectedDate(dates);
-        if (dates && dates.length > 0) {
-            const selectedDateString = dates[0].toISOString().split('T')[0]; // 첫 번째 선택된 날짜만 사용
-            const data = chartData.find(
-                (item) => item.date === selectedDateString
-            );
-            setSelectedData(data);
-        } else {
-            setSelectedData(undefined);
-        }
-    };
+    useEffect(() => {
+        const initialSelectedDates: Date[] = chartData.map(
+            (item) => new Date(item.date)
+        );
+        setSelectedDate(initialSelectedDates);
+    }, []);
 
-    const initialSelectedDates = chartData.map((item) => new Date(item.date));
-    console.log('initialSelectedDates :', initialSelectedDates);
+    const handleDayClick: DayMouseEventHandler = (day, _) => {
+        const selectedDateString = day.toISOString().split('T')[0];
+        console.log('selectedDateString:', selectedDateString);
+        chartData.forEach((item) => {
+            item.date === selectedDateString && setSelectedData(item);
+        });
+    };
     return (
         <section className="flex flex-col items-center">
             <Calendar
                 mode="multiple"
+                onDayClick={handleDayClick}
                 selected={selectedDate}
-                onSelect={handleDateChange} // dates는 Date[] | undefined
                 className="rounded-md border shadow"
             />
-            <div>선택된 날짜를 클릭해보세요</div>
-            {selectedDate && selectedData && (
+            {!selectedData && <div>날짜를 선택해주세요</div>}
+            {selectedDate.length > 0 && selectedData && (
                 <section>
                     <div>{selectedData.date}</div>
                     <div>
                         {selectedData.amount} 원 만큼
                         {selectedData.mood}한 날
                     </div>
-                    <div>메모: {selectedData.memo}</div>
+                    <div>{selectedData.memo}</div>
                 </section>
             )}
         </section>
