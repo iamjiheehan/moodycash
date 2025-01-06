c777440e70b655bfb3e080206889b70bcd33fe7c
한지희, Jihee Han <hatrix1014@gmail.com>
2025-01-06 23:57:13 +0900

    Resolved limitless loop error

--- components/service/ConfirmService.tsx
+++ components/service/ConfirmService.tsx
@@ -5,25 +5,17 @@ import { useServiceDetails } from '@/utils/store';
 import FormContainer from '@/components/form/FormContainer';
 import { SubmitButton } from '@/components/form/Buttons';
 import { createServiceAction } from '@/utils/actions';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
 
 function ConfirmService() {
     const { userId } = useAuth();
-    const { date, mood, price, description, resetServiceDetails } =
-        useServiceDetails((state) => ({
-            date: state.date,
-            mood: state.mood,
-            price: state.price,
-            description: state.description,
-            resetServiceDetails: state.resetServiceDetails,
-        }));
-
+    const { date, mood, price, description } = useServiceDetails(
+        (state) => state
+    );
     if (!userId)
         return (
             <SignInButton mode="modal">
                 <Button type="button" className="w-full">
-                    로그인이 필요합니다
+                    Sign In to Complete Booking
                 </Button>
             </SignInButton>
         );
@@ -38,9 +30,7 @@ function ConfirmService() {
         data.append('mood', mood);
         data.append('price', price.toString());
         data.append('description', description);
-        const result = await createServiceAction(null, data);
-
-        return result;
+        return await createServiceAction(null, data);
     };
 
     return (
